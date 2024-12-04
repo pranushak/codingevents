e7f1fec3fddf6d0a22e1535d9f6fabcb196b43f6
Pranusha <narsicloud@gmail.com>
2024-12-04 12:51:08 -0600

    created and displayed events in a form

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,9 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static  List<String> events = new ArrayList<>();
     @GetMapping
-    public String getAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("geeks for geeks");
-        events.add("code with pride");
-        events.add("launchcode education");
-        events.add("coursera");
+    public String displayAllEvents(Model model){
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,4 +25,9 @@ public class EventController {
         return "events/create";
     }
 
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events";
+    }
 }
