a4bcb719efd18cb4d60267f5e9d89b779ac894a2
Pranusha <narsicloud@gmail.com>
2024-12-16 14:49:47 -0600

    validation errors

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,7 +27,13 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad Data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
     }
